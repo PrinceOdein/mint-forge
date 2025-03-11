3970027d9ae285b711cc25760fe342c224d4c05b
PrinceOdein <odeinanyanwu@gmail.com>
2025-03-11 21:12:45 +0100

    commit for March 10th too tired
    Here

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { vars } = require("hardhat/config");
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: `https://polygon-amoy.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
